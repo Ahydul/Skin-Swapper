1009def106347a088fcd1937424fa35ce9e4e504
Ahydul <ahydul1@gmail.com>
2024-04-02 21:34:21 +0200

    Fix error in 3dskinlayers compatibility

--- src/main/java/net/cobrasrock/skinswapper/mixin/compatibility/skinlayers/ConfigAccessor.java
+++ src/main/java/net/cobrasrock/skinswapper/mixin/compatibility/skinlayers/ConfigAccessor.java
@@ -1,6 +1,6 @@
 package net.cobrasrock.skinswapper.mixin.compatibility.skinlayers;
 
-import dev.tr7zw.skinlayers.Config;
+import dev.tr7zw.skinlayers.versionless.config.Config;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.gen.Accessor;
